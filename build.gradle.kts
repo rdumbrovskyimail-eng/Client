822666a0880c1ac0552b065f30ab6352b9d3f6e1
rdumbrovskyimail-eng <r.dumbrovskyi.mail@gmail.com>
2026-09-12 18:02:59 +0200

    Update file

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,4 +5,5 @@ plugins {
     id("org.jetbrains.kotlin.plugin.compose") version "2.2.21" apply false
     id("com.google.devtools.ksp") version "2.2.21-2.0.4" apply false
     id("com.google.dagger.hilt.android") version "2.57.2" apply false
+    id("com.google.protobuf") version "0.9.4" apply false
 }
