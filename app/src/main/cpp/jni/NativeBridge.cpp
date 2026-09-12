5f6221c21e9716cd6fca8b7a28be7acdb07eae0a
rdumbrovskyimail-eng <r.dumbrovskyi.mail@gmail.com>
2026-09-12 18:37:47 +0200

    Update file

--- app/src/main/cpp/jni/NativeBridge.cpp
+++ app/src/main/cpp/jni/NativeBridge.cpp
@@ -1,47 +1,80 @@
+// >>> FILE: app/src/main/cpp/jni/NativeBridge.cpp
 #include <jni.h>
 #include <string>
 #include <android/log.h>
-#include <aaudio/AAudio.h>
+#include "audio/AAudioEngine.h"
 
-#define LOG_TAG "NativeCore"
+#define LOG_TAG "NativeCoreBridge"
 #define LOGI(...) __android_log_print(ANDROID_LOG_INFO, LOG_TAG, __VA_ARGS__)
-#define LOGE(...) __android_log_print(ANDROID_LOG_ERROR, LOG_TAG, __VA_ARGS__)
+
+using namespace client::audio;
 
 extern "C" JNIEXPORT jstring JNICALL
 Java_com_client_app_audio_NativeAudioBridge_getHardwareCoreInfo(JNIEnv *env, jobject /* this */) {
-    std::string info = "Qualcomm Snapdragon 8 Gen 2 (kalama) Native Engine [C++20/ARMv9] - AAudio Pipeline Ready";
-    LOGI("%s", info.c_str());
+    std::string info = "Qualcomm Snapdragon 8 Gen 2 (kalama) Native Engine [C++20/ARMv9] - AAudio Pipeline Active";
     return env->NewStringUTF(info.c_str());
 }
 
 extern "C" JNIEXPORT jboolean JNICALL
-Java_com_client_app_audio_NativeAudioBridge_probeMmapSupport(JNIEnv *env, jobject /* this */) {
-    bool mmapSupported = false;
-    AAudioStreamBuilder* builder = nullptr;
-    aaudio_result_t result = AAudio_createStreamBuilder(&builder);
-    
-    if (result == AAUDIO_OK && builder != nullptr) {
-        AAudioStreamBuilder_setPerformanceMode(builder, AAUDIO_PERFORMANCE_MODE_LOW_LATENCY);
-        AAudioStreamBuilder_setSharingMode(builder, AAUDIO_SHARING_MODE_EXCLUSIVE);
-        AAudioStreamBuilder_setDirection(builder, AAUDIO_DIRECTION_OUTPUT);
-        AAudioStreamBuilder_setFormat(builder, AAUDIO_FORMAT_PCM_I16);
-        AAudioStreamBuilder_setSampleRate(builder, 24000);
-        AAudioStreamBuilder_setChannelCount(builder, 1);
-        
-        AAudioStream* stream = nullptr;
-        result = AAudioStreamBuilder_openStream(builder, &stream);
-        if (result == AAUDIO_OK && stream != nullptr) {
-            aaudio_sharing_mode_t actualMode = AAudioStream_getSharingMode(stream);
-            aaudio_performance_mode_t perfMode = AAudioStream_getPerformanceMode(stream);
-            
-            mmapSupported = (actualMode == AAUDIO_SHARING_MODE_EXCLUSIVE && 
-                             perfMode == AAUDIO_PERFORMANCE_MODE_LOW_LATENCY);
-            
-            AAudioStream_close(stream);
-        }
-        AAudioStreamBuilder_delete(builder);
-    }
-    
-    LOGI("AAudio MMAP Hardware Probe: %s", mmapSupported ? "EXCLUSIVE_CONFIRMED" : "SHARED_FALLBACK");
-    return static_cast<jboolean>(mmapSupported);
+Java_com_client_app_audio_NativeAudioBridge_probeMmapSupport(JNIEnv * /* env */, jobject /* this */) {
+    return static_cast<jboolean>(AAudioEngine::getInstance().init());
+}
+
+extern "C" JNIEXPORT jboolean JNICALL
+Java_com_client_app_audio_NativeAudioBridge_startAudio(JNIEnv * /* env */, jobject /* this */) {
+    return static_cast<jboolean>(AAudioEngine::getInstance().start());
+}
+
+extern "C" JNIEXPORT void JNICALL
+Java_com_client_app_audio_NativeAudioBridge_stopAudio(JNIEnv * /* env */, jobject /* this */) {
+    AAudioEngine::getInstance().stop();
+}
+
+extern "C" JNIEXPORT jint JNICALL
+Java_com_client_app_audio_NativeAudioBridge_writePlaybackDirect(
+    JNIEnv *env, jobject /* this */, jobject byteBuffer, jint offsetBytes, jint lengthBytes) {
+
+    auto *bufferPtr = static_cast<int16_t*>(env->GetDirectBufferAddress(byteBuffer));
+    if (!bufferPtr) return 0;
+
+    auto *startPtr = reinterpret_cast<int16_t*>(reinterpret_cast<char*>(bufferPtr) + offsetBytes);
+    size_t frames = lengthBytes / sizeof(int16_t);
+
+    return static_cast<jint>(AAudioEngine::getInstance().writePlaybackPcm(startPtr, frames) * sizeof(int16_t));
+}
+
+extern "C" JNIEXPORT jint JNICALL
+Java_com_client_app_audio_NativeAudioBridge_readCaptureDirect(
+    JNIEnv *env, jobject /* this */, jobject byteBuffer, jint capacityBytes) {
+
+    auto *bufferPtr = static_cast<int16_t*>(env->GetDirectBufferAddress(byteBuffer));
+    if (!bufferPtr) return 0;
+
+    size_t maxFrames = capacityBytes / sizeof(int16_t);
+    return static_cast<jint>(AAudioEngine::getInstance().readCapturePcm(bufferPtr, maxFrames) * sizeof(int16_t));
+}
+
+extern "C" JNIEXPORT void JNICALL
+Java_com_client_app_audio_NativeAudioBridge_flushPlayback(JNIEnv * /* env */, jobject /* this */) {
+    AAudioEngine::getInstance().flushPlayback();
+}
+
+extern "C" JNIEXPORT void JNICALL
+Java_com_client_app_audio_NativeAudioBridge_setVolume(JNIEnv * /* env */, jobject /* this */, jfloat volume) {
+    AAudioEngine::getInstance().setVolume(volume);
+}
+
+extern "C" JNIEXPORT void JNICALL
+Java_com_client_app_audio_NativeAudioBridge_setMicGain(JNIEnv * /* env */, jobject /* this */, jfloat gain) {
+    AAudioEngine::getInstance().setMicGain(gain);
+}
+
+extern "C" JNIEXPORT void JNICALL
+Java_com_client_app_audio_NativeAudioBridge_getSpectrumData(JNIEnv *env, jobject /* this */, jfloatArray outArray) {
+    float bands[7] = {0.0f};
+    AAudioEngine::getInstance().getSpectrumUniforms(bands);
+    bands[5] = AAudioEngine::getInstance().getMicRms();
+    bands[6] = AAudioEngine::getInstance().getOutRms();
+
+    env->SetFloatArrayRegion(outArray, 0, 7, bands);
 }
