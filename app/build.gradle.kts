f75d1442d51049b4ff12df65b75ac403737a8348
rdumbrovskyimail-eng <r.dumbrovskyi.mail@gmail.com>
2026-09-12 18:03:17 +0200

    Update file

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -6,6 +6,7 @@ plugins {
     id("org.jetbrains.kotlin.plugin.compose")
     id("com.google.devtools.ksp")
     id("com.google.dagger.hilt.android")
+    id("com.google.protobuf")
 }
 
 android {
@@ -20,7 +21,31 @@ android {
         versionName = "1.0.0"
 
         ndk {
-            abiFilters.addAll(listOf("arm64-v8a"))
+            abiFilters.addAll(listOf("arm64-v8a", "x86_64"))
+        }
+
+        externalNativeBuild {
+            cmake {
+                arguments += listOf(
+                    "-DANDROID_STL=c++_shared",
+                    "-DANDROID_PLATFORM=android-26"
+                )
+                cppFlags += listOf(
+                    "-std=c++20",
+                    "-O3",
+                    "-march=armv8.2-a+fp16+dotprod",
+                    "-fvisibility=hidden",
+                    "-flto",
+                    "-ffast-math"
+                )
+            }
+        }
+    }
+
+    externalNativeBuild {
+        cmake {
+            path = file("src/main/cpp/CMakeLists.txt")
+            version = "3.22.1"
         }
     }
 
@@ -57,6 +82,21 @@ android {
     }
 }
 
+protobuf {
+    protoc {
+        artifact = "com.google.protobuf:protoc:3.25.5"
+    }
+    generateProtoTasks {
+        all().forEach { task ->
+            task.builtins {
+                maybeCreate("java").apply {
+                    option("lite")
+                }
+            }
+        }
+    }
+}
+
 kotlin {
     compilerOptions {
         jvmTarget.set(org.jetbrains.kotlin.gradle.dsl.JvmTarget.JVM_17)
@@ -79,29 +119,30 @@ dependencies {
     implementation("androidx.compose.ui:ui")
     implementation("androidx.compose.ui:ui-graphics")
     implementation("androidx.compose.material3:material3")
-    
-    // Защита: явная версия extended иконок вне BOM
     implementation("androidx.compose.material:material-icons-extended:1.7.8")
     implementation("androidx.compose.animation:animation")
     implementation("androidx.navigation:navigation-compose:2.8.5")
 
-    // Dagger Hilt (Синхронизирован с корнем 2.57.2)
+    // Dagger Hilt
     implementation("com.google.dagger:hilt-android:2.57.2")
     ksp("com.google.dagger:hilt-android-compiler:2.57.2")
     implementation("androidx.hilt:hilt-navigation-compose:1.2.0")
 
-    // Network & Data (Актуальные стабильные под Kotlin 2.2.21)
+    // Network & Serialization
     implementation("com.squareup.okhttp3:okhttp:4.12.0")
     implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.8.0")
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.10.1")
     implementation("androidx.datastore:datastore-preferences:1.1.2")
     implementation("com.jakewharton.timber:timber:5.0.1")
 
-    // Hardware & Media Stack (10/10)
+    // Hardware & Media Stack
     implementation("androidx.exifinterface:exifinterface:1.3.7")
-    // Критично для MediaSessionCompat (защита от засыпания Samsung One UI)
     implementation("androidx.media:media:1.7.0")
 
-    // Markdown рендеринг чата
+    // Markdown
     implementation("com.mikepenz:multiplatform-markdown-renderer-m3:0.27.0")
+
+    // Этап 1: Protobuf Lite & ONNX Runtime (Snapdragon HTP / QNN / NEON)
+    implementation("com.google.protobuf:protobuf-javalite:3.25.5")
+    implementation("com.microsoft.onnxruntime:onnxruntime-android:1.19.0")
 }
