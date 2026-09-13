2f2142bbaf380064b77f4ea3ee2912df27d6d49b
rdumbrovskyimail-eng <r.dumbrovskyi.mail@gmail.com>
2026-09-14 01:23:47 +0200

    Update file

--- app/src/main/java/com/client/app/ClientApplication.kt
+++ app/src/main/java/com/client/app/ClientApplication.kt
@@ -1,13 +1,21 @@
 package com.client.app
 
 import android.app.Application
+import com.client.app.logging.MemoryLogTree
 import dagger.hilt.android.HiltAndroidApp
 import timber.log.Timber
+import javax.inject.Inject
 
 @HiltAndroidApp
 class ClientApplication : Application() {
+
+    @Inject lateinit var memoryLogTree: MemoryLogTree
+
     override fun onCreate() {
         super.onCreate()
-        if (BuildConfig.DEBUG) Timber.plant(Timber.DebugTree())
+        Timber.plant(memoryLogTree)
+        if (BuildConfig.DEBUG) {
+            Timber.plant(Timber.DebugTree())
+        }
     }
 }
